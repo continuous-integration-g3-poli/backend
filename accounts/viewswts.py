7431850e87d55111f9d37ae54eb4004cfdd7b584
oscarwe <oscarortizalejandro2@outlook.com>
2025-05-26 17:51:10 -0500

    Add: Filtrado en modelo attendance por document

--- accounts/viewswts.py
+++ accounts/viewswts.py
@@ -4,22 +4,28 @@ from rest_framework.decorators import action
 from rest_framework import status
 from .models import Employee, AttendanceRecord
 from .serializers import EmployeeSerializer, AttendanceRecordSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import AttendanceRecordFilter
+
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__' 
+        
 class AttendanceRecordViewSet(viewsets.ModelViewSet):
     queryset = AttendanceRecord.objects.all()
     serializer_class = AttendanceRecordSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AttendanceRecordFilter
 
-    # Puedes agregar un endpoint para filtrar asistencia por fecha o empleado si quieres
     @action(detail=False, methods=['get'])
     def by_date(self, request):
         date = request.query_params.get('date')
         if not date:
             return Response({'error': 'Date parameter is required.'}, status=status.HTTP_400_BAD_REQUEST)
-        records = self.queryset.filter(date=date)
+        
+        records = self.get_queryset().filter(date=date)
         serializer = self.get_serializer(records, many=True)
         return Response(serializer.data)
